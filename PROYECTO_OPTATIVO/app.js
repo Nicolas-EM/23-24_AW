86e662bad22c3fee1af3664456edcce27d7d343c
Santiago Moral Santorio <santimoralsantorio@gmail.com>
2023-10-14 11:52:59 +0200

    ejs works

--- PROYECTO_OPTATIVO/app.js
+++ PROYECTO_OPTATIVO/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+const mime = require('mime');
+
 const path = require('path');
 const mysql = require('mysql');
 const DAO = require('./DAO');
@@ -13,31 +15,31 @@ const pool = mysql.createPool({
 const app = express();
 const Dao = new DAO(pool);
 app.set("view engine", "ejs");
-app.set("views", "./views");//pending review!! (!)
+app.set("views", path.join(__dirname, "views"));//pending review!! (!)
 /////////////
 
  /////////////
 
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/index',function(req, res) {
-   Dao.getdestinos(function(err, destinos){
-       if(err){
-           console.log(err);
-       }
-       else{
-           res.render("index", {destinations: destinos});
-       }
-   });
-});
-
+app.use(express.static(path.join(__dirname,"css")));
+app.use(express.static(path.join(__dirname,"resources")));
 // Start the server
 app.listen(3000, () => {
     console.log('Server listening on port 3000!');
 });
-
+console.log("pre get");
+app.get('/',function(req, res) {
+    Dao.getDestinos(function(err, destinos){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(destinos);
+            res.render("index", {destinations: destinos});
+        }
+    });
+ });
 //jpg to hex converter:
 function imageToHex() {
     const bitmap = fs.readFileSync('./resources/vacation_1.jpg');
