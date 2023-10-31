84fddb4e6122d9c9f778d2c614f7ff6e2d5d4dc7
Nicolas-EM <nico.mooser@gmail.com>
2023-10-31 17:32:46 +0100

    cositas
    Co-authored-by: Santiago Moral Santorio <sanord8@users.noreply.github.com>

--- PROYECTO_OPTATIVO/js/destino.js
+++ PROYECTO_OPTATIVO/js/destino.js
@@ -1,3 +1,14 @@
-document.onload(e => {
+$('#calendar').daterangepicker({
+    autoUpdateInput: false,
+    locale: {
+        cancelLabel: 'Clear'
+    },
+    "minDate": new Date(),
+}, function (start, end, label) {
+    console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+    //Handle price locally:
+    // let days = start.diff(b,'days')+1;
+    // console.log(days);
+    // $('#precioTotal').text(($('#precioTotal').text()) * days);
     
-})
+});
