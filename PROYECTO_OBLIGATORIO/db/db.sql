43d54d0074652c062c881fd7b592b2be031b442e
Santiago Moral Santorio <santimoralsantorio@gmail.com>
2023-11-29 17:07:02 +0100

    init de Practica obligatoria
    Co-authored-by: Nicolas Espinosa Mooser <nico.mooser@protonmail.com>

---
+++ PROYECTO_OBLIGATORIO/db/db.sql
@@ -0,0 +1,35 @@
+CREATE TABLE UCM_AW_RIU_USU_Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    faculty VARCHAR(255) NOT NULL,
+    grade VARCHAR(255) NOT NULL,
+    group VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    profileImage BLOB,
+    isAdmin BOOLEAN DEFAULT FALSE,
+    validated BOOLEAN DEFAULT FALSE,
+    UNIQUE (email)
+);
+
+
+CREATE TABLE UCM_AW_RIU_INS_Facilities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    availabity VARCHAR(255) NOT NULL,
+    type VARCHAR(12) NOT NULL,
+    capacity INT,
+    image BLOB
+);
+
+CREATE TABLE UCM_AW_RIU_RES_Reservations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dateini DATETIME NOT NULL,
+    dateend DATETIME NOT NULL,
+    datecreation DATETIME NOT NULL,
+    userid INT NOT NULL,
+    instid INT NOT NULL,
+    FOREIGN KEY (usuid) REFERENCES UCM_AW_RIU_USU_Usuarios(id),
+    FOREIGN KEY (instid) REFERENCES UCM_AW_RIU_INS_Instalaciones(id)
+);
---
+++ PROYECTO_OBLIGATORIO/endpoints.md
@@ -0,0 +1,38 @@
+# Endpoints
+
+
+```
+riu.ucm.es
+├── /admin*
+│   └── /config 
+├── /users
+│   ├── /
+│   ├── /create
+│   ├── /update
+│   ├── /delete
+│   ├── /login
+│   ├── /logout
+│   ├── /role*
+│   ├── /validate*
+│   └── /byFaculty*
+├── /reservations
+│   ├── /create
+│   ├── /update
+│   ├── /delete
+│   ├── /byUser*
+│   └── /byFaculty*
+├── /installations
+│   ├── /create*
+│   ├── /delete*
+│   ├── /modify*
+│   ├── /get
+│   └── /search
+├── /faculties
+│   ├── /create*
+│   ├── /update*
+│   ├── /delete*
+│   └── /get
+└── /messages
+    ├── /send
+    └── /get
+```
