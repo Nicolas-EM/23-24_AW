da8f3552a7cbd0fb19bd1835742cf6a36a023b94
Nicolas Espinosa Mooser <nico.mooser@gmail.com>
2023-12-09 18:20:26 +0100

    messages in progress

--- PROYECTO_OBLIGATORIO/db/DAOMessages.js
+++ PROYECTO_OBLIGATORIO/db/DAOMessages.js
@@ -8,7 +8,7 @@ class DAOMessages {
     //los mensajes donde el usuario es el destinatario y no el emisor
     getMessagesByReceiver(userId, callback) {
         this.pool.query(
-            "SELECT * FROM ucm_aw_riu_messages WHERE receiver_id = ?",
+            "SELECT m.*, u.name AS sender_name, u.surname AS sender_surname, u.email AS sender_email FROM ucm_aw_riu_messages m JOIN ucm_aw_riu_usu_users u ON m.sender_id = u.id WHERE m.receiver_id = ? ORDER BY m.timestamp ASC;",
             [userId],
             (err, rows) => {
                 if (err) {
